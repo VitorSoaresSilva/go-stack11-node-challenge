a995cea6d44251278c0dc65d28bb1fe81706996c
Vitor Soares Silva <vitorcabeca2005@hotmail.com>
2020-08-19 15:23:08 -0300

    Update Repositories

--- src/app.js
+++ src/app.js
@@ -24,7 +24,16 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id == id);
+  if(repoIndex < 0){
+    return response.status(400).json({error : "Repositorie not found"})
+  }
+  const {likes} = repositories[repoIndex]
+  const {title, url, techs} = request.body;
+  const repo = {id, url, title, techs, likes};
+  repositories[repoIndex] = repo;
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
